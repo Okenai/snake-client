5283aae4ab3512234a966ae3ab8f6576e3065a38
Lana <kassembayeva@gmail.com>
2021-06-02 22:02:59 +0000

    the client file is created

---
+++ client.js
@@ -0,0 +1,26 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: '192.168.0.139', //"135.23.222.131"
+    port: 50542
+  })
+
+  conn.setEncoding("utf8");
+  
+  conn.on('connect', ()=>{
+    console.log('Successfully connected to game server')
+  })
+  
+  conn.write('Name: KKR');
+
+  conn.on('data', (data)=>
+  console.log(data));
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,20 +1,3 @@
 const net = require('net');
+const connect = require('./client');
 
-const connect = function() {
-  const conn = net.createConnection({
-    host: "135.23.222.131",
-    port: 50542
-  })
-
-  conn.setEncoding("utf8");
-
-  conn.on('data', (data)=>
-  console.log(data));
-
-  return conn;
-};
-
-
-
-console.log("Connecting ...");
-connect();
