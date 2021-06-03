1fc0c526369ad3dccd877a63d8e65cd42a42cf6a
Lana <kassembayeva@gmail.com>
2021-06-03 02:38:36 +0000

    the wasp keys were passed into handleInput function to write to server to act on

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -11,6 +14,18 @@ const setupInput = function () {
 const handleUserInput = function(input) {
   if (input === '\u0003') {
     process.exit();
+  }
+  if (input === 'w') {
+    connection.write('Move: up');
+  }
+  if (input === 'a') {
+    connection.write('Move: left');
+  }
+  if (input === 's') {
+    connection.write('Move: down');
+  }
+  if (input === 'd') {
+    connection.write('Move: right');
   }
 }
 
